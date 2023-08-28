3d9a36b923812fa72b648c4bd32acf8727355234
IgorLezhanin <lezhanin@gmail.com>
2023-08-29 02:59:44 +0300

    задание 7

---
+++ lesson1/lesson_1_task_7.py
@@ -0,0 +1,41 @@
+def print1():
+    print (1)
+
+def print2():
+    print (2)
+
+def print3():
+    print (3)
+
+def print4():
+    print (4)
+
+def print5():
+    print (5)
+
+def print6():
+    print (6)
+
+def print7():
+    print (7)
+
+def print8():
+    print (8)
+
+def print9():
+    print (9)
+
+def print0():
+    print (0)
+
+print8()
+print8()
+print0()
+print0()
+print5()
+print5()
+print5()
+print3()
+print5()
+print3()
+print5()
